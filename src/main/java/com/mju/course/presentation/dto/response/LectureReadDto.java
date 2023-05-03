415ec7afd418c73ccd2e4335b484c7a30b73040d
sangminee <lsmlee99@gmail.com>
2023-05-04 04:45:03 +0900

    [#3] feat : 코스 조회 api에 swagger 추가

--- src/main/java/com/mju/course/presentation/dto/response/LectureReadDto.java
+++ src/main/java/com/mju/course/presentation/dto/response/LectureReadDto.java
@@ -1,6 +1,7 @@
 package com.mju.course.presentation.dto.response;
 
 import com.mju.course.domain.model.Lecture;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,11 +10,17 @@ import lombok.Getter;
 @AllArgsConstructor
 @Builder
 public class LectureReadDto {
+    @Schema(description = "강의 인덱스", defaultValue = "1")
     private Long lectureIndex;
+    @Schema(description = "강의 순서", defaultValue = "1")
     private int lectureSequence;
+    @Schema(description = "강의 제목", defaultValue = "1")
     private String lectureTitle;
+    @Schema(description = "강의 시간 (분)", defaultValue = "1")
     private int lectureTime;
+    @Schema(description = "강의 url", defaultValue = "1")
     private String lectureUrl;
+    @Schema(description = "강의 설명", defaultValue = "1")
     private String lectureDescription;
 
     public static LectureReadDto of(Lecture lecture){
@@ -22,7 +29,7 @@ public class LectureReadDto {
                 .lectureSequence(lecture.getLectureSequence())
                 .lectureTitle(lecture.getLectureTitle())
                 .lectureTime(lecture.getLectureTime())
-                .lectureUrl(lecture.getLectureUrl())
+                .lectureUrl("https://d19wla4ff811v8.cloudfront.net/" + lecture.getLectureUrl())
                 .lectureDescription(lecture.getLectureDescription())
                 .build();
     }
