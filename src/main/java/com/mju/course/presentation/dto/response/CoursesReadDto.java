7a6dcc57fb4bc4eb18134e62bff5a9f7e0e08514
sangminee <lsmlee99@gmail.com>
2023-05-04 03:56:13 +0900

    [#3] feat : 코스 목록 request dto에 Swagger3 추가

--- src/main/java/com/mju/course/presentation/dto/response/CoursesReadDto.java
+++ src/main/java/com/mju/course/presentation/dto/response/CoursesReadDto.java
@@ -1,6 +1,7 @@
 package com.mju.course.presentation.dto.response;
 
 import com.mju.course.domain.model.Course;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,10 +11,15 @@ import lombok.Getter;
 @Builder
 public class CoursesReadDto {
 
+    @Schema(description = "코스 인덱스", defaultValue = "1")
     private Long courseIndex;
+    @Schema(description = "코스 이름", defaultValue = "자바 기초")
     private String courseName;
+    @Schema(description = "가격", defaultValue = "100000")
     private String price;
+    @Schema(description = "난이도", defaultValue = "2")
     private int difficulty;
+    @Schema(description = "코스 기본 사진")
     private String courseTitlePhotoUrl;
 
     public static CoursesReadDto of(Course course){
