c13c5da35d36f9e9f2d33f621d7081168991f34f
sangminee <lsmlee99@gmail.com>
2023-05-04 05:24:17 +0900

    [#3] feat : (강사) 코스 삭제 - 완전 삭제 api 구현

--- src/main/java/com/mju/course/domain/model/other/Exception/ExceptionList.java
+++ src/main/java/com/mju/course/domain/model/other/Exception/ExceptionList.java
@@ -8,15 +8,23 @@ import lombok.RequiredArgsConstructor;
 public enum ExceptionList {
 
     UNKNOWN(-9999, "알 수 없는 오류가 발생하였습니다."),
-    NOT_EXISTENT_COURSE(-5001, "존재 하지 않는 코스입니다."),
-    NOT_EXISTENT_CURRICULUM(-5001, "존재 하지 않는 커리 쿨럼입니다."),
-    DUPLICATION_COURSE_NAME(-5002, "중복된 코스 이름입니다."),
-    DUPLICATION_LECTURE(-5002, "이미 존재하는 강의 입니다."),
-    EXCEEDED_LECTURE_SEQUENCE(-5004, "해당 커리큘럼의 강의 수를 초과했습니다. 커리큘럼 정보 수정 후 다시 등록해주세요."),
-    NO_MODIFIED_COURSE(-5005, "수정된 코스 요소가 없습니다."),
 
-    NO_MODIFIED_LECTURE(-5005, "수정된 강의 요소가 없습니다."),
-    NOT_EXISTENT_LECTURE(-5005, "존재 하지 않는 강의입니다.");
+    // 코스
+    NOT_EXISTENT_COURSE(-5001, "존재 하지 않는 코스입니다."),
+    DUPLICATION_COURSE_NAME(-5002, "중복된 코스 이름입니다."),
+    NO_MODIFIED_COURSE(-5003, "수정된 코스 요소가 없습니다."),
+
+    // 커리 큘럼
+    NOT_EXISTENT_CURRICULUM(-5011, "존재 하지 않는 커리 쿨럼입니다."),
+    EXCEEDED_LECTURE_SEQUENCE(-5012, "해당 커리큘럼의 강의 수를 초과했습니다. 커리큘럼 정보 수정 후 다시 등록해주세요."),
+
+    // 강의
+    DUPLICATION_LECTURE(-5021, "이미 존재하는 강의 입니다."),
+    NO_MODIFIED_LECTURE(-5022, "수정된 강의 요소가 없습니다."),
+    NOT_EXISTENT_LECTURE(-5023, "존재 하지 않는 강의입니다."),
+
+    // s3
+    ERROR_S3_OBJECT_DELETE(-5031, "s3 객체 삭제를 실패했습니다.");
 
     private final int code;
     private final String message;
