cf1e6efdd945eda789874713a04b206bd09f150f
sangminee <lsmlee99@gmail.com>
2023-05-18 18:13:07 +0900

    [#16] fix : mapping 이름 변경

--- src/main/java/com/mju/course/infrastructure/config/Swagger3Config.java
+++ src/main/java/com/mju/course/infrastructure/config/Swagger3Config.java
@@ -17,7 +17,7 @@ public class Swagger3Config {
 
     @Bean
     public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/v1/**"};
+        String[] paths = {"/course-service/**","/lecture-service/**","/admin-service/**"};
 
         return GroupedOpenApi.builder()
                 .group("SoftCoA API v1")
