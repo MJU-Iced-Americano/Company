f1f19d95c59270a976009af20b3c052366a50cf8
sangminee <lsmlee99@gmail.com>
2023-05-22 05:51:06 +0900

    [#16] fix : merge 오류 고침

--- src/main/java/com/mju/course/infrastructure/config/WebConfig.java
+++ src/main/java/com/mju/course/infrastructure/config/WebConfig.java
@@ -6,10 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://54.180.213.187:8080", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
+
 }
