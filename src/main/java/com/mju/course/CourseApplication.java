fbfb529bd3da66cd091551f8473bb3f8209aeffc
sangminee <lsmlee99@gmail.com>
2023-05-29 05:18:35 +0900

    [#30] fix : spring-cloud-starter-openfeign 버전 추가

--- src/main/java/com/mju/course/CourseApplication.java
+++ src/main/java/com/mju/course/CourseApplication.java
@@ -3,11 +3,13 @@ package com.mju.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableFeignClients
 public class CourseApplication {
 
 	public static void main(String[] args) {
