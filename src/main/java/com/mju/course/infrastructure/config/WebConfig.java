6b86ccdf697c808385cf9d93670e56068fc84779
sangminee <lsmlee99@gmail.com>
2023-06-02 03:51:56 +0900

    [#30] fix : cors 고침

--- src/main/java/com/mju/course/infrastructure/config/WebConfig.java
+++ src/main/java/com/mju/course/infrastructure/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.180.213.187:8080", "http://localhost:3000")
+                .allowedOrigins("*")
+//                .allowedOrigins("http://54.180.213.187:8080", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 
